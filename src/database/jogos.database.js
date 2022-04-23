b781c8c240e1d93a4a070e2a9035213d0caea3a5
JVictor1604 <josevictor8569@gmail.com>
2022-04-22 21:54:00 -0300

    commit

--- src/database/jogos.database.js
+++ src/database/jogos.database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 function connectToDatabase() {
   mongoose
-    .connect('mongodb+srv://Victor:zDhxbm9gIGEnnrFb@apirick-and-morty.mq3aj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    .connect('mongodb://localhost:27017/Jogos-DB', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
